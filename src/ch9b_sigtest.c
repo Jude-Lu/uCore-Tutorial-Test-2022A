78ec5be24717b47648a261b827f47acb95dbb9eb
Lu Jun <jun.lu@csu.edu.cn>
2022-12-03 19:47:30 +0800

    Update signal test

--- src/ch9b_sigtest.c
+++ src/ch9b_sigtest.c
@@ -5,42 +5,31 @@
 #include <unistd.h>
 
 int sigio = 0;
-int sigkill = 0;
 
-void handler(int signum, void *info, void *context)
+void handler()
 {
-	if(signum == SIGIO) {
-		sigio++;
-		printf("SIGIO   signal: %d\n", signum);
-	}
-	else if(signum == SIGKILL) {
-		sigkill++;
-		printf("SIGKILL   signal: %d\n", signum);
-	}
-	else
-		printf("error\n");
+    sigio++;
+    sigreturn();
 }
 
 int main()
 {
-	sigact act;
+    sigact act;
 
-	act.handler = handler;
-	act.sa_mask = SA_SIGINFO;
+    act.handler = handler;
+    act.sa_mask = SA_SIGINFO;
 
-	puts("start sigtest\n");
-	printf("hello sigtest\n");
-	sigaction(SIGIO, &act, NULL);
-	sigaction(SIGKILL, &act, NULL);
+    puts("start sigtest\n");
+    printf("hello sigtest\n");
+    sigaction(SIGIO, &act, NULL);
+    assert_eq(sigaction(SIGKILL, &act, NULL), -1);
 
-	sleep(1000);
-	kill(getpid(), SIGIO);
-	sleep(1000);
-	kill(getpid(), SIGKILL);
-	sleep(1000);
+    sleep(1000);
+    for (int i = 0;i < 100;i++)
+        kill(getpid(), SIGIO);
+    sleep(1000);
 
-	assert_eq(sigio, 1);
-	assert_eq(sigkill, 1);
-	puts("sigtest passed!");
-	return 0;
+    assert_eq(sigio, 100);
+    puts("sigtest passed!");
+    return 0;
 }
